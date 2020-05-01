e8e326f98d63546064178fc8dcb55fc523ec254b
bcheema007 <acheema@mail.greenriver.edu>
2020-04-30 17:37:03 -0700

    Added Changes

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const h1 = document.createElement('h1'); //output the tip amount
 document.body.appendChild(h1);
 
+const percentInput = document.createElement('input');
+percentInput.setAttribute('type', 'number');
+percentInput.setAttribute('placeholder', 'Enter tip percentage');
+document.body.appendChild(percentInput);
+
 const input = document.createElement('input');
 input.setAttribute('type', 'number');
+input.setAttribute('placeholder', 'Enter amount');
+input.id = "amount";
 document.body.appendChild(input);
 
 const btn = document.createElement('button');
@@ -12,9 +19,10 @@ document.body.appendChild(btn);
 btn.addEventListener('click', getTipAmount);
 
 function getTipAmount() {
-    let cost = (input.value * 0.15).toFixed(2); //round to two decimal places
+    let cost = (input.value * (percentInput.value / 100)).toFixed(2); //round to two decimal places
     h1.innerHTML = `You should tip $${cost} on $${input.value}`;
     input.value = "";
+    percentInput.value = "";
 }
 
 
